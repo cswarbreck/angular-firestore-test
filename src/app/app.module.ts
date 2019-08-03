968298a7ec152cb9f01276917db0fd94a72b69a3
Cosmo Swarbreck <c.swarbreck@gmail.com>
2019-08-03 18:21:39 +0100

    Imports cars component and items service into app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { CarsComponent } from './components/cars/cars.component';
+import { ItemsService } from './services/items.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'angular-firestore-test'),
     AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ItemsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
