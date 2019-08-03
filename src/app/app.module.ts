199af3d534c527faeb20973eef40d7975d0fa02c
Cosmo Swarbreck <c.swarbreck@gmail.com>
2019-08-03 18:13:40 +0100

    Adds in firestore config to app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'angular-firestore-test'),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCj1xHHtv83jCHsYsIGxniFh6zJy3VNXQw',
+    authDomain: 'cars-tech-test-9ca15.firebaseapp.com',
+    databaseURL: 'https://cars-tech-test-9ca15.firebaseio.com',
+    projectId: 'cars-tech-test-9ca15',
+    storageBucket: 'cars-tech-test-9ca15.appspot.com',
+    messagingSenderId: '169669831514',
+    appId: '1:169669831514:web:742176b24ad9b218'
+  }
 };
 
 /*
