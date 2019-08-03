a4ebd6cb26b07dcb149add96dd7fde0be8fcd008
Cosmo Swarbreck <c.swarbreck@gmail.com>
2019-08-03 19:47:14 +0100

    Adds logic to return data from firestore with valueChanges()

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Item } from '../models/item';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ItemsService {
+  itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
-  constructor() { }
+  constructor(public afs: AngularFirestore) {
+    this.items = this.afs.collection('cars').valueChanges();
+  }
+
+  getItems() {
+    return this.items;
+  }
+
 }
+
