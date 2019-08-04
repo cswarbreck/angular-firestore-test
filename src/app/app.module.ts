ea54388aaa4d6db2d10b532056202053d451c44a
Cosmo Swarbreck <c.swarbreck@gmail.com>
2019-08-04 15:04:27 +0100

    Adds add-car component and formsmodule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
@@ -9,17 +10,20 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { CarsComponent } from './components/cars/cars.component';
 import { ItemsService } from './services/items.service';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddCarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'cars-tech-test'),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule,
   ],
   providers: [ItemsService],
   bootstrap: [AppComponent]
